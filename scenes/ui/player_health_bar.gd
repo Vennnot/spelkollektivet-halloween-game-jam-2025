48fa2cfbf5d66cada884b4a9ae21754d688d05ca
dan-linux-laptop <vennnot@gmail.com>
2025-10-31 10:22:15 +0100

    health anim

--- scenes/ui/player_health_bar.gd
+++ scenes/ui/player_health_bar.gd
@@ -1,6 +1,9 @@
 class_name PlayerHealthBar extends Control
 
-const HEART := preload("uid://bpv3hmigfp77f")
+const HEART = preload("uid://b1tprn422ubsc")
+const TILE_001 = preload("uid://cwo3cwinhk03")
+const TILE_002 = preload("uid://c4ss7lurdpq0p")
+
 
 @onready var heart_container: HBoxContainer = %HeartContainer
 @export var heart_texture: Texture2D
@@ -16,7 +19,11 @@ func set_healthbar():
 func on_player_health_changed(new_value: int):
 	if new_value >= get_health_containers().size():
 		return
-	var container :TextureRect= heart_container.get_children().pop_front()
+	var container :TextureRect= heart_container.get_children().pop_back()
+	container.texture = TILE_001
+	await get_tree().create_timer(0.25).timeout
+	container.texture = TILE_002
+	await get_tree().create_timer(0.25).timeout
 	container.queue_free()
 
 
